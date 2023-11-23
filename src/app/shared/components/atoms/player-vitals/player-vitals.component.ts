3f8145a0c7a8945e19f469cb5b2630e8fef4989c
jboude <boudejerome@gmail.com>
2023-11-23 23:39:34 +0100

    call PlayerService to get Player object

--- src/app/shared/components/atoms/player-vitals/player-vitals.component.ts
+++ src/app/shared/components/atoms/player-vitals/player-vitals.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PlayerService } from '../../../../core/services/player.service';
+import { Player } from '../../../../core/model/player';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-player-vitals',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './player-vitals.component.scss'
 })
 export class PlayerVitalsComponent {
+  player: Player | null = null;
+  private playerSubscription: Subscription | undefined;
+  
+  constructor(private playerService: PlayerService) {
+  }
 
+  ngOnInit(): void {
+    this.playerSubscription = this.playerService.player$.subscribe(player => {
+      this.player = player;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.playerSubscription?.unsubscribe();
+  }
 }
