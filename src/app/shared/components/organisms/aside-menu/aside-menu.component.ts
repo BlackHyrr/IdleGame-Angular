ebb9b85c7b2ef34a60dd396b4aa336cde8e191e6
jboude <boudejerome@gmail.com>
2023-11-23 23:42:16 +0100

    call PlayerService to get Player object

--- src/app/shared/components/organisms/aside-menu/aside-menu.component.ts
+++ src/app/shared/components/organisms/aside-menu/aside-menu.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Player } from '../../../../core/model/player';
+import { PlayerService } from '../../../../core/services/player.service';
+import { Subscription, take } from 'rxjs';
+import { WindowStateService } from '../../../../core/services/window-state.service';
 
 @Component({
   selector: 'app-aside-menu',
@@ -7,4 +11,28 @@ import { Component } from '@angular/core';
 })
 export class AsideMenuComponent {
 
+  player: Player | null = null;
+  private playerSubscription: Subscription | undefined;
+  
+  constructor(private playerService: PlayerService, private windowStateService: WindowStateService) {}
+
+  toggleWindow(windowId: string): void {
+    this.windowStateService.openWindow$.pipe(take(1)).subscribe((currentWindow) => {
+      if (currentWindow === windowId) {
+        this.windowStateService.closeWindow();
+      } else {
+        this.windowStateService.openWindow(windowId);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.playerSubscription = this.playerService.player$.subscribe(player => {
+      this.player = player;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.playerSubscription?.unsubscribe();
+  }
 }
