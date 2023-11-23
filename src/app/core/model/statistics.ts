9b6db7dd2cde549e60077529ca7f7948ea46d331
jboude <boudejerome@gmail.com>
2023-11-23 23:33:48 +0100

    add various properties

--- src/app/core/model/statistics.ts
+++ src/app/core/model/statistics.ts
@@ -1,5 +1,10 @@
-interface StatisticsInterface {
+export interface StatisticsInterface {
     health: number;
     mana: number;
     stamina: number;
+    maxHealth: number;
+    maxMana: number;
+    maxStamina: number;
+    inventorySlots: number;
+    [key: string]: number;
 }
