9ad17221712a196909a63be1903c63c390820ae0
jboude <boudejerome@gmail.com>
2023-11-23 10:30:16 +0100

    add component containg player resources components

---
+++ src/app/shared/components/atoms/player-vitals/player-vitals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-vitals',
+  templateUrl: './player-vitals.component.html',
+  styleUrl: './player-vitals.component.scss'
+})
+export class PlayerVitalsComponent {
+
+}
