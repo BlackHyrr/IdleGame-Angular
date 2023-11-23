337396d64fc8086c64f81e81bdeb34b07faebef1
jboude <boudejerome@gmail.com>
2023-11-23 23:43:25 +0100

    call PlayerService to get Player object

--- src/app/shared/components/organisms/header/header.component.ts
+++ src/app/shared/components/organisms/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component, EventEmitter, Output  } from '@angular/core';
+import { Component } from '@angular/core';
 import { MainMenuService } from '../../../../core/services/main-menu.service';
+import { Player } from '../../../../core/model/player';
+import { PlayerService } from '../../../../core/services/player.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,12 +10,24 @@ import { MainMenuService } from '../../../../core/services/main-menu.service';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  player: Player | null = null;
+  private playerSubscription: Subscription | undefined;
+
+  constructor(private mainMenuService: MainMenuService, private playerService: PlayerService) {
+    
+  }
 
   handleMainMenuButtonClick(): void {
     this.mainMenuService.toggleMainMenu();
   }
 
-  constructor(private mainMenuService: MainMenuService) {
-    
+  ngOnInit(): void {
+    this.playerSubscription = this.playerService.player$.subscribe(player => {
+      this.player = player;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.playerSubscription?.unsubscribe();
   }
 }
