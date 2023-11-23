fbb8e6639cb7bdb528fc6a04cc7b9f68d177ea0a
jboude <boudejerome@gmail.com>
2023-11-23 23:37:52 +0100

    call PlayerService to get Player object

--- src/app/shared/components/atoms/player-identity/player-identity.component.ts
+++ src/app/shared/components/atoms/player-identity/player-identity.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PlayerService } from '../../../../core/services/player.service';
+import { Player } from '../../../../core/model/player';
 
 @Component({
   selector: 'app-player-identity',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './player-identity.component.scss'
 })
 export class PlayerIdentityComponent {
+  player: Player | null = null;
+  private playerSubscription: Subscription | undefined;
+  
+  constructor(private playerService: PlayerService) {
+  }
 
+  ngOnInit(): void {
+    this.playerSubscription = this.playerService.player$.subscribe(player => {
+      this.player = player;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.playerSubscription?.unsubscribe();
+  }
 }
