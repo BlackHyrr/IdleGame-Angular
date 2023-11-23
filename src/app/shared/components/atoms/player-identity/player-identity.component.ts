6227878fc32b4e5b37ee908e6d632d503493581b
jboude <boudejerome@gmail.com>
2023-11-23 10:29:36 +0100

    add component for player name and title

---
+++ src/app/shared/components/atoms/player-identity/player-identity.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-identity',
+  templateUrl: './player-identity.component.html',
+  styleUrl: './player-identity.component.scss'
+})
+export class PlayerIdentityComponent {
+
+}
