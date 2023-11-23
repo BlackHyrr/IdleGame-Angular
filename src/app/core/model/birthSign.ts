cf7ab3b30f16bfbcd45c709b75a2629e17eb939e
jboude <boudejerome@gmail.com>
2023-11-23 10:33:30 +0100

    add interface and type birthsigns

---
+++ src/app/core/model/birthSign.ts
@@ -0,0 +1,7 @@
+type BirthSign = 'Aries' | 'Taurus' | 'Gemini' | 'Cancer' | 'Leo' | 'Virgo' | 'Libra' | 'Scorpio' | 'Sagittarius' | 'Capricorn' | 'Aquarius' | 'Pisces';
+
+interface BirthSignStatsInterface {
+    health?: number;
+    mana?: number;
+    stamina?: number;
+}
