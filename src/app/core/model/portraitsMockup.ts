ec62de0d45eae2612ad77dd3a0c05e19923d385a
jboude <boudejerome@gmail.com>
2023-11-23 23:33:25 +0100

    add a list of portait images

---
+++ src/app/core/model/portraitsMockup.ts
@@ -0,0 +1,13 @@
+export const portraits: string[] = [
+    "./assets/images/portrait/0001",
+    "./assets/images/portrait/0002",
+    "./assets/images/portrait/0003",
+    "./assets/images/portrait/0004",
+    "./assets/images/portrait/0005",
+    "./assets/images/portrait/0006",
+    "./assets/images/portrait/0007",
+    "./assets/images/portrait/0008",
+    "./assets/images/portrait/0009",
+    "./assets/images/portrait/0010",
+    "./assets/images/portrait/0011"
+]
