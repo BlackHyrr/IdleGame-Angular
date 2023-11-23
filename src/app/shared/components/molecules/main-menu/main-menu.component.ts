ea1abe733554d42a149b73784a38aeea4664fae8
jboude <boudejerome@gmail.com>
2023-11-23 10:30:53 +0100

    add component for the pause/main menu

---
+++ src/app/shared/components/molecules/main-menu/main-menu.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MainMenuService } from '../../../../core/services/main-menu.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.scss'
+})
+export class MainMenuComponent implements OnInit{
+  menuVisible$: Observable<boolean> = this.mainMenuService.menuVisible$;
+  @Input() buttonType: string = '';
+
+  handleButtonClick(buttonType: string): void {
+    switch (buttonType) {
+      case 'main-menu':
+        this.mainMenuService.toggleMainMenu();
+        break;
+      case 'resume':
+        this.mainMenuService.toggleMainMenu();
+        break
+      // case 'save':
+      //   break;
+      // case 'load':
+      //   break;
+      // case 'options':
+      //   break;
+      // case 'quit':
+      //   break;
+      default:
+        throw new Error(`This button is invalid. Check the buttonType attribute for the components.`)
+    }
+  }
+
+  constructor(private mainMenuService: MainMenuService) {}
+
+  ngOnInit(): void {
+    this.menuVisible$ = this.mainMenuService.menuVisible$;
+  }
+}
