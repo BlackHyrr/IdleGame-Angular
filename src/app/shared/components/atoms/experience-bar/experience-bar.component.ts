2ce7d19e297e74af931834e60aac22d3c42760bc
jboude <boudejerome@gmail.com>
2023-11-23 10:28:39 +0100

    add the experience bar component

---
+++ src/app/shared/components/atoms/experience-bar/experience-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-bar',
+  templateUrl: './experience-bar.component.html',
+  styleUrl: './experience-bar.component.scss'
+})
+export class ExperienceBarComponent {
+
+}
