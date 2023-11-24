3bcabeeeb2e395d4529aa820439bb6f8262c7aab
jboude <boudejerome@gmail.com>
2023-11-24 13:36:04 +0100

    change property to max[property]

--- src/app/core/model/birthSign.ts
+++ src/app/core/model/birthSign.ts
@@ -1,7 +1,7 @@
 export type BirthSign = 'Aries' | 'Taurus' | 'Gemini' | 'Cancer' | 'Leo' | 'Virgo' | 'Libra' | 'Scorpio' | 'Sagittarius' | 'Capricorn' | 'Aquarius' | 'Pisces';
 
 export interface BirthSignStatsInterface {
-    health?: number;
-    mana?: number;
-    stamina?: number;
+    maxHealth?: number;
+    maxMana?: number;
+    maxStamina?: number;
 }
