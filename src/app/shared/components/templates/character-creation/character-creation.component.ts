81f860a7d353b46e49c08aa9e8ba2537b509d775
jboude <boudejerome@gmail.com>
2023-11-24 13:39:17 +0100

    load service to get portraits

--- src/app/shared/components/templates/character-creation/character-creation.component.ts
+++ src/app/shared/components/templates/character-creation/character-creation.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Player, PlayerInterface, playerDefaultStats } from '../../../../core/model/player';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { BirthSign } from '../../../../core/model/birthSign';
-import { portraits } from '../../../../core/model/portraitsMockup';
 import { PlayerService } from '../../../../core/services/player.service';
 import { WindowStateService } from '../../../../core/services/window-state.service';
 import { take } from 'rxjs';
+import { PortraitService } from '../../../../core/services/portrait.service';
 
 
 @Component({
@@ -13,26 +13,31 @@ import { take } from 'rxjs';
   templateUrl: './character-creation.component.html',
   styleUrl: './character-creation.component.scss'
 })
-export class CharacterCreationComponent implements OnInit{
+export class CharacterCreationComponent implements OnInit {
 
   birthsignsList: BirthSign[] = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'];
   formSubmitted: boolean = false;
-  @Input() portraits: string[] = portraits;
+  @Input() portraits: string[] = [];
   @Input() portraitLarge: string = '';
 
-  characterForm = new FormGroup({
-    name: new FormControl('', {validators: [Validators.required, Validators.minLength(3), Validators.maxLength(25)], nonNullable: true}) ?? '' , 
-    rank: new FormControl('', {nonNullable: true}),
-    portraitSmall: new FormControl('', {validators:[Validators.required], nonNullable: true}),
-    portraitLarge: new FormControl('', {validators:[Validators.required], nonNullable: true}),
-    titles: new FormControl<string[]>([], {nonNullable: true}),
-    birthSign: new FormControl('Aries', {validators:[Validators.required], nonNullable: true}),
-  })
+  constructor(
+    private playerService: PlayerService,
+    protected windowStateService: WindowStateService,
+    private portraitService: PortraitService) {
+  }
 
-  constructor(private playerService: PlayerService, protected windowStateService: WindowStateService) {}
+  characterForm = new FormGroup({
+    name: new FormControl('', { validators: [Validators.required, Validators.minLength(3), Validators.maxLength(25)], nonNullable: true }) ?? '',
+    rank: new FormControl('', { nonNullable: true }),
+    portraitSmall: new FormControl('', { validators: [Validators.required], nonNullable: true }),
+    portraitLarge: new FormControl('', { validators: [Validators.required], nonNullable: true }),
+    titles: new FormControl<string[]>([], { nonNullable: true }),
+    birthSign: new FormControl('Aries', { validators: [Validators.required], nonNullable: true }),
+  })
 
   ngOnInit(): void {
     this.windowStateService.setDefaultWindow('creationMenu');
+    this.portraits = this.portraitService.getPortraits();
   }
 
   selectPortrait(portrait: string): void {
@@ -66,10 +71,12 @@ export class CharacterCreationComponent implements OnInit{
       const playerData: PlayerInterface = { ...defaultValues, ...formData } as PlayerInterface;
 
       const player = new Player(playerData);
+      player.updateStatsBasedOnBirthSign();
       this.playerService.setPlayer(player);
       this.formSubmitted = true;
     }
     else {
+      this.characterForm.markAllAsTouched();
       console.log('form invalid');
     }
   }
